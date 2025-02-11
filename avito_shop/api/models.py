94696963b177c286fdacf3edde606b389e399fe6
belyashnikovatn <101242667+belyashnikovatn@users.noreply.github.com>
2025-02-11 18:45:45 +0500

    feat: recreate User model and make migrations

--- avito_shop/api/models.py
+++ avito_shop/api/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    coins = models.IntegerField(default=1000, verbose_name='Монеты')
+
+    class Meta:
+        verbose_name = 'сотрудник'
+        verbose_name_plural = 'Сотрудники'
+
+    def __str__(self):
+        return self.username
