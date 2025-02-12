fde39b2fab233737969e6389fadad2d42c30a154
belyashnikovatn <101242667+belyashnikovatn@users.noreply.github.com>
2025-02-12 11:39:36 +0500

    feat: set admin for models

--- avito_shop/api/admin.py
+++ avito_shop/api/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
-
+from api.models import Buy, Gift, Merch
 User = get_user_model()
 
 
@@ -11,3 +11,18 @@ class UserAdmin(admin.ModelAdmin):
 
     list_display = ('username', 'coins',)
     search_fields = ('username',)
+
+
+@admin.register(Buy)
+class BuyAdmin(admin.ModelAdmin):
+    """Admin Buy"""
+
+
+@admin.register(Gift)
+class GiftAdmin(admin.ModelAdmin):
+    """Admin Gift"""
+
+
+@admin.register(Merch)
+class MerchAdmin(admin.ModelAdmin):
+    """Admin Merch"""
