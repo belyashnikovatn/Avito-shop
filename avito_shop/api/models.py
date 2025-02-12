5a450cf158057ec73235f49164518e5342e53f82
belyashnikovatn <101242667+belyashnikovatn@users.noreply.github.com>
2025-02-12 11:20:58 +0500

    feat: add all models & migrations

--- avito_shop/api/models.py
+++ avito_shop/api/models.py
@@ -3,12 +3,17 @@ from django.db import models
 
 
 class Profile(AbstractUser):
+    """Профиль сотрудника: имя + монеты."""
+
     username = models.CharField(
         verbose_name='Никнейм',
         unique=True,
         max_length=255,
     )
-    coins = models.IntegerField(default=1000, verbose_name='Монеты')
+    coins = models.IntegerField(
+        default=1000,
+        verbose_name='Монеты'
+    )
 
     class Meta:
         verbose_name = 'сотрудник'
@@ -16,3 +21,77 @@ class Profile(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Merch(models.Model):
+    """Мерч на складе."""
+
+    name = models.SlugField(
+        verbose_name='Название',
+        unique=True,
+        max_length=200,
+    )
+    price = models.IntegerField(
+        verbose_name='Цена',
+    )
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return f'{self.name} за {self.price}'
+
+
+class Gift(models.Model):
+    """Подарок монет сотруднику."""
+
+    from_user = models.ForeignKey(
+        Profile,
+        related_name='sender',
+        on_delete=models.CASCADE,
+        verbose_name='Даритель',
+    )
+    to_user = models.ForeignKey(
+        Profile,
+        related_name='receiver',
+        on_delete=models.CASCADE,
+        verbose_name='Получатель',
+    )
+    amount = models.IntegerField(
+        verbose_name='Количество',
+    )
+    created = models.DateTimeField(
+        'Дата операции', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'подарок'
+        verbose_name_plural = 'Подарки'
+
+    def __str__(self):
+        return f'{self.from_user} подарил {self.to_user} {self.amount}'
+
+
+class Buy(models.Model):
+    """Покупка мерча."""
+
+    user = models.ForeignKey(
+        Profile,
+        related_name='buyer',
+        on_delete=models.CASCADE,
+        verbose_name='Покупатель'
+    )
+    merch = models.ForeignKey(
+        Merch,
+        related_name='shopping',
+        on_delete=models.CASCADE,
+        verbose_name='Мерч')
+    created = models.DateTimeField(
+        'Дата операции', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'покупка'
+        verbose_name_plural = 'Покупки'
+
+    def __str__(self):
+        return f'{self.user} купил {self.merch}'
